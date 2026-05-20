309551fdf2bc20ae4e6cd4f40769bd5f339f7bb7
rtritto <ariele.1994@gmail.com>
2026-05-20 21:30:26 +0200

    feat(vite): add target plugin

---
+++ packages/vite/src/plugins/target.ts
@@ -0,0 +1,50 @@
+import type { BuildEnvironmentOptions, Plugin } from "vite";
+
+export interface TargetPluginOptions {
+  /**
+   * The entry point to your custom server implementation.
+   * e.g. `'./server/entrypoint.ts'`
+   */
+  entry: string;
+}
+
+/**
+ * A generic target plugin that overrides the server entry with a custom wrapper.
+ */
+export function target(options: TargetPluginOptions): Plugin[] {
+  return [
+    {
+      name: "ud:target:emit",
+      apply: "build",
+      config: {
+        order: "post",
+        handler() {
+          const buildEnvOptions: BuildEnvironmentOptions = {};
+          if (this.meta.rolldownVersion) {
+            buildEnvOptions.rolldownOptions = {
+              input: {
+                index: options.entry,
+              },
+            };
+          } else {
+            buildEnvOptions.rollupOptions = {
+              input: {
+                index: options.entry,
+              },
+            };
+          }
+
+          return {
+            environments: {
+              ssr: {
+                build: {
+                  ...buildEnvOptions,
+                },
+              },
+            },
+          };
+        },
+      },
+    },
+  ];
+}
